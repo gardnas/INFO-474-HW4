9f0a2983e7249ee16d680d17cb71ed2625f14827
gardnas <sdgard863@gmail.com>
2025-10-22 16:18:04 -0700

    sketch 1: added green rectangle to represent a chalkboard. made the canvas size 800 by 800 as per the requirements

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,14 +1,16 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
   p.setup = function () {
-    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.createCanvas(800, 800);
   };
   p.draw = function () {
     p.background(220);
-    p.fill(100, 150, 240);
-    p.textSize(32);
-    p.textAlign(p.CENTER, p.CENTER);
-    p.text('HWK #4. A', p.width / 2, p.height / 2);
+    // dark green background:
+    //  https://www.figma.com/colors/dark-green/    
+    p.fill(6, 64, 43);
+    p.noStroke();
+    p.rect(50,50, 700, 500);
+    
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 });
