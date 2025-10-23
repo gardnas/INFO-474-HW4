052e441e79fd34740e3204c9ebf964fdd1549fdf
gardnas <sdgard863@gmail.com>
2025-10-22 18:23:11 -0700

    sketch 1: added brown border to make it look like a chalkboard

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -5,11 +5,17 @@ registerSketch('sk2', function (p) {
   };
   p.draw = function () {
     p.background(220);
-    // dark green background:
-    //  https://www.figma.com/colors/dark-green/    
+    // dark green background: https://www.figma.com/colors/dark-green/    
+    // chalkboard
     p.fill(6, 64, 43);
     p.noStroke();
     p.rect(50,50, 700, 500);
+
+    // brown border
+    p.noFill();
+    p.strokeWeight(20);
+    p.stroke(120, 80, 40);
+    p.rect(50, 50, 700, 500);
     
   };
   p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
